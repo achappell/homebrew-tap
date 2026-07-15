2abada9ca636473009860a606c48f757bce5e4d6
Amanda Chappell <achappell@me.com>
2026-07-15 17:04:53 -0500

    Update openusage to 0.24.1

--- Formula/openusage.rb
+++ Formula/openusage.rb
@@ -3,27 +3,27 @@
 
 class Openusage < Formula
   desc "Monitor your AI coding tool quotas from a single TUI dashboard"
-  homepage "https://openusage.sh"
-  version "0.23.0"
+  homepage "https://github.com/achappell/openusage"
+  version "0.24.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/janekbaraniewski/openusage/releases/download/v0.23.0/openusage_0.23.0_darwin_arm64.tar.gz"
-      sha256 "10523164bbe403e1c223faa1bf335324517cf17dd4c7b233596ec1b8686939c5"
+      url "https://github.com/achappell/openusage/releases/download/v0.24.1/openusage_0.24.1_darwin_arm64.tar.gz"
+      sha256 "8ac29ffd77c7359160d6ba88e70dee97183abb3b07e0356260ac63087af129c2"
     else
-      url "https://github.com/janekbaraniewski/openusage/releases/download/v0.23.0/openusage_0.23.0_darwin_amd64.tar.gz"
-      sha256 "ceaa2155dc2a127ac36ee56ce164a9adf11c44c666ef9a0e5f881d66a36d8106"
+      url "https://github.com/achappell/openusage/releases/download/v0.24.1/openusage_0.24.1_darwin_amd64.tar.gz"
+      sha256 "b4b4d8cb54af8c01b04de105d1101e4b950247ccd2c2a8951801bf618ad2768a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/janekbaraniewski/openusage/releases/download/v0.23.0/openusage_0.23.0_linux_arm64.tar.gz"
-      sha256 "7e93f7fdd6dffc320b5e7a8bad33d79042ed26719b0ed02365ccedde61996c1d"
+      url "https://github.com/achappell/openusage/releases/download/v0.24.1/openusage_0.24.1_linux_arm64.tar.gz"
+      sha256 "3fa714cd69a3908337185b061b19892476a420f54699e277e3fe5d59cc64de29"
     else
-      url "https://github.com/janekbaraniewski/openusage/releases/download/v0.23.0/openusage_0.23.0_linux_amd64.tar.gz"
-      sha256 "ac2da8b4cd89b09d9a4a2d6b0cf6c30494272ba8a40719aaaa5a0470a9613cce"
+      url "https://github.com/achappell/openusage/releases/download/v0.24.1/openusage_0.24.1_linux_amd64.tar.gz"
+      sha256 "690877e04a35fc2f752f265c1dd83ca117557ac6f2dcdbab74b439a4664bebdc"
     end
   end
 
@@ -40,7 +40,7 @@ class Openusage < Formula
 
       Trust the whole tap once to keep openusage linked across upgrades:
 
-        brew trust janekbaraniewski/tap
+        brew trust achappell/tap
 
       See https://docs.brew.sh/Tap-Trust for details.
     EOS
